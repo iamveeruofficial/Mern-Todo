609ffed45415832006a7722c2f9acc80b89bf7e1
Naru Gopal <veeruk0217@gmail.com>
2023-04-17 09:06:52 +0530

    commit

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-require("dotenv").config({ path: "config.env" });
+require('dotenv').config({ path: "config.env" });
 const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
 
-const routes = require("./router/router");
 
+
+const routes = require("./router/router");
 
 app.use(cors());
 
-// app.use(
-//   cors({
-//     origin: process.env.FRONTEND_URL,
-//     credentials: true,
-//   })
-// );
-
-require("./db/conn");
+require('./db/conn');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended:false}));
 
 const PORT = process.env.PORT || 5000;
 
@@ -28,17 +22,16 @@ app.use(routes);
 
 app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
-    function (err) {
-      if (err) {
-        res.status(500).send(err);
-      }
-    }
-  );
-});
+app.get('*', function(_, res){
+    res.sendFile(
+        path.join(__dirname, "./client/build/index.html"),
+        function (err){
+            if(err){
+                res.status(500).send(err);
+            }
+        }
+    )
+})
 
-app.listen(PORT, () =>
-  console.log("Server running on port http://localhost:" + PORT)
-);
+
+app.listen(PORT, () => console.log("Server running on port http://localhost:"+ PORT));
